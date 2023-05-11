6fdf242101790a7cd66a9d518428b663bb87d103
hAppser <vip313z@ukr.net>
2023-05-11 19:33:33 +0300

    added AI and end of the game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import setBoard from "./lib/useCreateBoards";
-import "./App.scss";
+import setBoard from "./lib/setBoard";
 import Field from "./components/Field/Field";
+import "./App.scss";
 
 //TO DO
 
@@ -9,17 +9,17 @@ import Field from "./components/Field/Field";
 // 1.1) Регистрация попаданий (done)
 // 1.2) Смена стилей после стрельбы (done)
 
-// 2) Интеллект бота
+// 2) Интеллект бота (done, примитивной интеллект добавлен)
 
 // 3) Передача хода
 
-// 4) Конец игры
+// 4) Конец игры (done, но в примитивной форме)
 
 function App() {
   const [playerBoard, setPlayerBoard] = useState(setBoard());
-  const [enemyBoard, setEnemyBoard] = useState(setBoard());
-  const [playerHits, setPlayerHits] = useState(1);
-  const [computerHits, setComputerHits] = useState(0);
+  const [computerBoard, setComputerBoard] = useState(setBoard());
+  const [playerHits, setPlayerHits] = useState(19);
+  const [computerHits, setComputerHits] = useState(19);
   const [winner, setWinner] = useState(" ");
 
   const handleFire = (value, rowIndex, colIndex) => {
@@ -27,23 +27,40 @@ function App() {
       return;
     }
     if (value === 1) {
-      const newBoard = [...enemyBoard];
+      const newBoard = [...computerBoard];
       newBoard[0][rowIndex][colIndex] = 2;
-      setEnemyBoard(newBoard);
-      setPlayerHits(playerHits + 1);
-      if (playerHits + 1 === 20) {
+      setComputerBoard(newBoard);
+      setPlayerHits(playerHits - 1);
+      if (playerHits === 0) {
         setWinner("player");
       }
     } else if (value === 0) {
-      const newBoard = [...enemyBoard];
-      newBoard[0][rowIndex][colIndex] = 3;
-      setEnemyBoard(newBoard);
-      if (value === 1) {
-        setComputerHits(computerHits + 1);
-        if (computerHits === 20) {
-          setWinner("computer");
+      const computerFire = () => {
+        let newBoard = [...computerBoard];
+        newBoard[0][rowIndex][colIndex] = 3;
+        setComputerBoard(newBoard);
+        newBoard = [...playerBoard];
+        let row = Math.floor(Math.random() * 10);
+        let col = Math.floor(Math.random() * 10);
+        if (newBoard[0][row][col] === 2 || newBoard[0][row][col] === 3) {
+          return;
         }
-      }
+
+        if (newBoard[0][row][col] === 1) {
+          setComputerHits(computerHits - 1);
+          newBoard[0][row][col] = 2;
+          setPlayerBoard(newBoard);
+          if (computerHits === 0) {
+            setWinner("computer");
+          }
+          computerFire();
+        } else {
+          newBoard[0][row][col] = 3;
+          setPlayerBoard(newBoard);
+          return;
+        }
+      };
+      computerFire();
     }
   };
   return (
@@ -51,7 +68,7 @@ function App() {
       <h1>real naval battle</h1>
       <main>
         <Field
-          board={enemyBoard}
+          board={computerBoard}
           onClick={(value, rowIndex, colIndex) =>
             handleFire(value, rowIndex, colIndex)
           }
@@ -63,6 +80,11 @@ function App() {
             handleFire(value, rowIndex, colIndex)
           }
         />
+        {winner === "player" ? (
+          <h1>Победа игрока!</h1>
+        ) : winner === "computer" ? (
+          <h1>Победа компьютера!</h1>
+        ) : null}
       </main>
     </div>
   );
